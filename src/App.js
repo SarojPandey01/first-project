83c91c3d84b2ec0c7b781dfc57afde1112b82674
SarojPandey01 <96424735+SarojPandey01@users.noreply.github.com>
2023-09-09 20:09:55 +0545

    first project review

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import TodoFirebase from "./components/TodoFirebase";
+import TodoFirebase from "./components/Todofirebase.jsx";
+import Todo from "./components/Todo";
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Todo /> */}
+      <Todo />
+      <br/><br/><br/><br/><br/>
       <TodoFirebase />
     </div>
   );
